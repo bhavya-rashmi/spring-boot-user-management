2339e2b08132d8dc42cc98f7d77e104513abdad2
bhavya-rashmi <rashmibhavya721@gmail.com>
2026-02-11 20:02:55 +0530

    Initial commit

---
+++ src/main/java/com/UserManagement/Entity/User.java
@@ -0,0 +1,126 @@
+package com.UserManagement.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+
+public class User {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private Integer age;
+
+    @Column(nullable=false)
+    private String phone;
+
+    @Column(nullable=false)
+    private String gender;
+
+    @Column(nullable=false)
+    private String address;
+
+    @Column(nullable=false)
+    private String password;
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	@ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+
+    private List<Role> roles = new ArrayList<>();
+
+}
---
+++ src/main/java/com/UserManagement/Repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.UserManagement.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.UserManagement.Entity.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+   User  findByEmail(String email);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
